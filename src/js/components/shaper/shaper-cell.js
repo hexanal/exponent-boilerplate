8fb089db18cf4f86f8bc4ea0fe453a9da01e1ac0
Fred Mercy <fred.mercy@cossette.com>
2020-06-07 16:29:18 -0400

    saving work... switching computers... meh

--- src/js/components/shaper/shaper-cell.js
+++ src/js/components/shaper/shaper-cell.js
@@ -6,29 +6,43 @@ export default ({element}) => {
   const state = {
     lock: false,
     drawn: false,
-    type: 'round'
+    type: 'center-center',
+    lockType: 'center-center'
   };
+
+  // todo: give access to optimization AS YOU GO
+  // don't START with them necessarily: KNOW THAT THEY'RE THERE
+  // when optimizing -> knowledge that heavy processing is happening that requires optimizing
 
   const hotCornerSize = getComputedStyle(element).getPropertyValue('--hot-corner-size');
   const CORNER_THRESHOLD = parseInt( hotCornerSize.replace('%', ''), 10 ) / 100;
 
   element.addEventListener('mousedown', () => {
-    shaperState.set('mode', (state.drawn ? 'erase' : 'draw') );
-
-    state.drawn = !state.drawn;
-    element.dataset.drawn = state.drawn;
+    shaperState
+      .set('mode', (state.drawn ? 'edit' : 'draw') )
+      .and(mode => {
+        state.mode = mode;
+        state.drawn = state.mode === 'edit' ? !state.drawn : true;
+        render();
+      });
   });
   element.addEventListener('mouseenter', e => {
     const mode = shaperState.get('mode');
 
     if (mode === 'off') return;
 
-    state.drawn = mode === 'draw'; // the other possible mode is 'erase', which set the cell to empty :)
-    element.dataset.drawn = state.drawn;
+    state.drawn = mode === 'draw';
+    render();
+  });
+  element.addEventListener('mouseleave', e => {
+    const mode = shaperState.get('mode');
+
+    if (mode === 'off') return;
+
+    state.drawn = mode === 'draw';
+    render();
   });
   element.addEventListener('mousemove', e => {
-    if ( state.drawn && shaperState.get('mode') === 'off' ) return; // don't alter this cell if it's drawn and we are not currently drawing or erasing
-
     const { top, left, width, height } = element.getBoundingClientRect();
     const ratioY = (e.clientY - top) / height;
     const ratioX = (e.clientX - left) / width;
@@ -38,7 +52,12 @@ export default ({element}) => {
     const type = vertical + '-' + horizontal;
 
     state.type = type;
-    element.dataset.type = state.type;
+    render();
   });
+
+  function render() {
+    element.dataset.drawn = state.drawn;
+    element.dataset.type = state.type;
+  }
 }
 
